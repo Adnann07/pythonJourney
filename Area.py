894423989ac6b004c9e423ffc4d5a4ab020b36a9
Adnan <64307271+Adnann07@users.noreply.github.com>
2024-08-01 15:55:47 +0600

    Add files via upload

---
+++ Area.py
@@ -0,0 +1,15 @@
+from math import *
+radius=float(input("enter radius: "))
+print(radius*3.1416)
+
+base=float(input("enter base: "))
+height=float(input("enter height: "))
+print(0.5*base*height)
+
+print(max(20,10))
+print(pow(2,3))
+num1=20
+num2=30
+print(f"{num1}+{num2}")
+print("adnan",end="")
+print("01729998600")
---
+++ newPython.py
@@ -0,0 +1,7 @@
+print("adnan \n 123")
+name=input("enter name: ")
+
+cg= int(input("enter cg: "))
+age= int(input("enter age: "))
+print("hello "+name)
+print("your cg: ",cg)
